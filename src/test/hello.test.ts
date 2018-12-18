1f5d4a95c67f50793308b7e920f17c0c0500de9e
Emmanuel <ogbiyoyosky@yahoo.com>
2018-12-18 10:32:05 +0000

    Add functionality for unit and coverage test

---
+++ src/test/hello.test.ts
@@ -0,0 +1,12 @@
+import hello from '../api/hello';
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+import 'mocha';
+require('ts-mocha');
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello World!');
+  });
+});
