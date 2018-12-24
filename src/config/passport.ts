a5a4d46c4ddb45c98d61a6589edc70d21ba36b1a
Emmanuel <ogbiyoyosky@yahoo.com>
2018-12-24 04:22:52 +0000

    Add documentation and formating

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,19 +1,24 @@
 let JwtStrategy = require('passport-jwt').Strategy;
 let ExtractJwt = require('passport-jwt').ExtractJwt;
 
-import UserSchema from '../api/user/user.model';  
- 
+import UserSchema from '../api/user/user.model';
+
 /** Check if the JWT matches an user in the database. */
-module.exports = function(passport) { 
-  let opts = {jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'), secretOrKey: process.env.SECRET|| "miracle123"};
+module.exports = function (passport) {
+  let opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey: process.env.SECRET || "miracle123"
+  };
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    UserSchema.findOne({_id: jwt_payload._id}, (err, user) => {
-    	if(err){
+    UserSchema.findOne({
+      _id: jwt_payload._id
+    }, (err, user) => {
+      if (err) {
         return done(err, false);
       }
-      if(user){
+      if (user) {
         done(null, user);
-      }else{
+      } else {
         done(null, false);
       }
     });
