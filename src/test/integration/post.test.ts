0ee1f9a91c80866c0056292320f2b6fda14c7f12
Emmanuel <ogbiyoyosky@yahoo.com>
2018-12-29 19:42:47 +0000

    Add test for to get a post in the database

--- src/test/integration/post.test.ts
+++ src/test/integration/post.test.ts
@@ -74,6 +74,34 @@ describe('Post', ()=> {
         })
     })
 
+    describe('GET /api/v1/posts/id', ()=> {
+
+        it('Should return a post from the db when passed the id as param', ()=> {
+            const post = new PostModel({
+                author: 'ogbiyoyo',
+                permlink : 'https://steemit.com/@sirfreeman',
+                postImage: 'https://steemit.com/@sirfreeman',
+                postDescription : 'hello post',
+                postTitle : 'hello post',
+                Tags: ['fashlogue', 'contempatory'],
+                postedTo: ['fashlogue', 'steemblockchain']  
+            })
+            post.save(post =>{
+             request(app)
+             .get('/api/v1/users'+ post.user._id)
+             .expect(httpStatus.OK)
+             .then(res =>
+                expect(res.body.result)
+                .to
+                .be
+                .an('object')
+             )
+            })
+         })
+ 
+    })
+
+
     describe('PUT api/v1/posts/:_id', ()=> {
         it('it should edit a post in the database with the id', () => {
             const post = new PostModel({
